62611eb16a9780584b14dfe8ce380a93fd8decf7
Poluianov Valeriy <bestcom5055@gmail.com>
2025-02-13 21:41:19 +0300

    Fix: Hide SECRET_KEY into the env

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### HH Test task 01. 
 API Service, returning "Hello, {name}! {message}!" by GET request.
+
+#### Installation
+
+Set up the `/.env` file, example (`/config_example.env`):
+```bash
+DJANGO_SECRET_KEY = "django-insecure-v@+elu9*2@n*n_-ui_#)f+cl)bn_90np^h2*Hello:)y$=A2f8)e"
+```
--- hello_api/hello_api/settings.py
+++ hello_api/hello_api/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from os import getenv
+
+
+# Load SECRET_KEY from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cpahj5cjx2w1!95%3o@w954yldx-x4zy519n7^uomh^%6&mvcl'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
